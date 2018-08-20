15b9197b3db978a41eaaea2106b7ca2cb6b77b5d
Jacob Moberg <moberg_17@hotmail.com>
2018-08-20 20:08:29 +0200

    Added DiceRoll.py

---
+++ DiceRoll.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import random
+min = 1
+max = 6
+rollAgain = "yes"
+
+while rollAgain == "y" or rollAgain == "yes" :
+    print("\nDice rolled: " + str(random.randint(min, max)) + "!\n")
+    rollAgain = raw_input("Wanna roll again? ")
